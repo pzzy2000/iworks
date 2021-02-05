ab50fc8094ad928e6c6b7861b4c5ec61a3edb404
Administrator <Administrator@192.168.0.144>
2021-02-06 01:35:23 +0800

    i

---
+++ cn.oxo.iworks.web/src/main/java/cn/oxo/iworks/web/spring/jackson/LongObjectMapper.java
@@ -0,0 +1,34 @@
+package cn.oxo.iworks.web.spring.jackson;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+public class LongObjectMapper extends ObjectMapper {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8365212110783780682L;
+
+	private Logger logger = LogManager.getLogger(LongObjectMapper.class);
+
+	public LongObjectMapper() {
+		init();
+	}
+
+	private void init() {
+
+		SimpleModule simpleModule = new SimpleModule();
+
+		simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+
+		simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+
+		registerModule(simpleModule);
+
+	}
+
+}
