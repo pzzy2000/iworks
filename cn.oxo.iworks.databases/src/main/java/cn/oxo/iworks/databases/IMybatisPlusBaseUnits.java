e2fd80bf801c75df5cb3fb33b39f6116b1bac237
zy <zy@zy.com>
2021-11-04 22:44:43 +0800

    ii

--- cn.oxo.iworks.databases/src/main/java/cn/oxo/iworks/databases/IMybatisPlusBaseUnits.java
+++ cn.oxo.iworks.databases/src/main/java/cn/oxo/iworks/databases/IMybatisPlusBaseUnits.java
@@ -1,18 +1,22 @@
 package cn.oxo.iworks.databases;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
 public abstract class IMybatisPlusBaseUnits<Bean> extends ABBaseUnits{
 
 	public abstract BaseMapper<Bean> getBaseMapper();
 	
 
-    @Autowired
-    @Qualifier(IPlatformRedisService.name)
-    protected IPlatformRedisService platformRedisService;
+//    @Autowired
+//    @Qualifier(IPlatformRedisService.name)
+//    protected IPlatformRedisService platformRedisService;
 
     public LambdaQueryWrapper<Bean> create() {
         LambdaQueryWrapper<Bean> queryWrapper = Wrappers.lambdaQuery();
--- cn.oxo.iworks.databases/src/main/java/org/uorm/ISearch.java
+++
@@ -1,9 +0,0 @@
-package org.uorm;
-
-import java.io.Serializable;
-
-public interface ISearch {
-
-    public Serializable search(Class<Serializable> clazz, String idFields, Serializable idValue) throws Exception;
-
-}
